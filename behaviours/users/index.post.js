5e8bd1fd3b158c5573afaa56bb6d9a51ee7a8cf8
Andrej Sýkora <as@andrejsykora.com>
2018-02-03 17:40:07 +0100

    fix - create user with password

--- behaviours/users/index.post.js
+++ behaviours/users/index.post.js
@@ -1,7 +1,14 @@
+const crypto = require('crypto')
+
 const queries = require('../../model/queries')
 
 module.exports = async function (ctx, next) {
   const newUser = ctx.request.body
+
+  // Create password hash
+  const hash = crypto.createHash('sha1')
+  hash.update(newUser.password)
+  newUser.password = hash.digest('hex')
 
   await queries.user.create({'$user': newUser}, ctx.sqlAdapter)
 
